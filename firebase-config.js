ee573188dfbc29141524e0ea1aa747c76f1440b5
y0892100-pixel <y0892100@gmail.com>
2026-08-30 03:55:26 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCXYzZgFIFU2u6R02_ConvSonD9SJRWS9Q",
-  authDomain: "varen-f7756.firebaseapp.com",
-  projectId: "varen-f7756",
-  storageBucket: "varen-f7756.firebasestorage.app",
-  messagingSenderId: "648684291842",
-  appId: "1:648684291842:web:f3e806fc76a6e119caa41b",
-  measurementId: "G-SV7DKGPH2M"
+  apiKey: "ضع هنا apiKey الخاص بك",
+  authDomain: "ضع هنا authDomain",
+  projectId: "ضع هنا projectId",
+  storageBucket: "ضع هنا storageBucket",
+  messagingSenderId: "ضع هنا messagingSenderId",
+  appId: "ضع هنا appId"
 };
-
 export default firebaseConfig;
