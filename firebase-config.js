32d960133dde3ebcb6868fa13e8506d8e66c4c76
y0892100-pixel <y0892100@gmail.com>
2026-08-29 20:15:53 +0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCXYzZgFIFU2u6RB2_ConvSonD9SJRWS9Q",
+  authDomain: "varen-f7756.firebaseapp.com",
+  projectId: "varen-f7756",
+  storageBucket: "varen-f7756.appspot.com",
+  messagingSenderId: "648684291842",
+  appId: "1:648684291842:web:f3e886fc76a6e119caa41b",
+  measurementId: "G-SV7DKGPH2M"
+};
+
+export default firebaseConfig;
